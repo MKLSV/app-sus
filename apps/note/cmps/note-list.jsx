3e391ef75aa4c8cf20e6c75dfea1221f3ad1cce0
alexokin <alex.okin@gmail.com>
2022-12-28 15:58:49 +0200

    submission - 16:00

--- apps/note/cmps/note-list.jsx
+++ apps/note/cmps/note-list.jsx
@@ -7,8 +7,12 @@ export function NoteList() {
 
     return <section className="note-list">
         <ul>
-            <li ><NotePreview /></li>
-            <Link to={`/note/234`}>Select Note</Link> 
+        <Link to={`/note/1`}><li ><NotePreview /></li></Link> 
+        <Link to={`/note/2`}><li ><NotePreview /></li></Link> 
+        <Link to={`/note/3`}><li ><NotePreview /></li></Link> 
+        <Link to={`/note/4`}><li ><NotePreview /></li></Link> 
+        <Link to={`/note/5`}><li ><NotePreview /></li></Link> 
+        <Link to={`/note/6`}><li ><NotePreview /></li></Link> 
             {/* change 234 to note id */}
         </ul>
         
--- apps/note/views/note-index.jsx
+++ apps/note/views/note-index.jsx
@@ -1,13 +1,30 @@
-import { NoteList } from "../cmps/note-list.jsx"
+const { useState, useEffect} = React
 
+
+import { NoteList } from "../cmps/note-list.jsx";
+import { noteService } from "../services/note.service.js";
+import { storageService } from "../../../services/async-storage.service.js";
 
 export function NoteIndex() {
+  const [notes, setNotes] = useState([]);
 
-    return <section className="note-index">
-        <h1>note app</h1>
-        <input type="text" className="note-search-bar"/>
-        
-        <NoteList />
+useEffect(() => {
+  loadNotes()
+
+}, [])
+
+console.log(notes);
+
+
+function loadNotes() {
+    noteService.query().then(notes => setNotes(notes))
+}
+  return (
+    <section className="note-index">
+      <h1>Note App</h1>
+      <input type="text" className="note-search-bar" />
+
+      <NoteList />
     </section>
-
+  );
 }
