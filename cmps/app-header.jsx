c828370cd2da581e484cfa031f200328b47c85d7
MKLSV <mkolosov96@gmail.com>
2022-12-29 17:48:46 +0300

    nav menu done

--- cmps/app-header.jsx
+++ cmps/app-header.jsx
@@ -1,16 +1,21 @@
 const { Link, NavLink } = ReactRouterDOM
+const { useState } = React
 
 export function AppHeader() {
+
+    const [isExpanted, setIsExpanted] = useState(false)
 
     return <header className="app-header">
         <Link to="/">
             <h3>LOGO!</h3>
         </Link>
-        <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/mail">Mail</NavLink>
-            <NavLink to="/note">Note</NavLink>
+        <nav className={isExpanted ? 'nav-bar on-menu' : 'nav-bar'}>
+            <NavLink to="/">Home<i className="fa-solid fa-house"></i></NavLink>
+            <NavLink to="/about">About <i className="fa-solid fa-question"></i></NavLink>
+            <NavLink to="/mail">Mail <i className="fa-solid fa-envelope"></i></NavLink>
+            <NavLink to="/note">Note <i className="fa-solid fa-note-sticky"></i></NavLink>
         </nav>
+        <a className="menu-toggle" onClick={() => setIsExpanted(!isExpanted)}><i className="fa-solid fa-bars"></i></a>
+
     </header>
 }
