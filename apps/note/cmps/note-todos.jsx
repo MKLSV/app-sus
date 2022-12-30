4d771b11827a16f2a3ff92059b77926472358c4c
alexokin <alex.okin@gmail.com>
2022-12-30 11:29:47 +0200

    css fixing

--- apps/note/cmps/note-todos.jsx
+++ apps/note/cmps/note-todos.jsx
@@ -11,3 +11,37 @@ export function NoteTodos({ info }) {
     </div>
   );
 }
+
+
+// <div
+//   className={`note ${note.type}`}
+//   style={{
+//     backgroundColor: note.style.backgroundColor,
+//     fontFamily: note.style.fontFamily,
+//   }}
+// >
+//   <i className="fa fa-list-ul"></i>
+//   <div style={{ fontSize: note.style.fontSize + "px" }}>
+//     {note.info.label && <h5 className="note-label">{note.info.label}</h5>}
+//     {note.info.valxue.map((todo, idx) => {
+//       return (
+//         <p key={id}>
+//           <li
+//             onClick={() => {
+//               eventBusService.emit("markTodo", { note, idx });
+//             }}
+//             className={todo.isDone ? "mark" : ""}
+//           >
+//             {todo.txt}
+//           </li>{" "}
+//           {todo.doneAt && (
+//             <span>Done At : {new Date(todo.doneAt).toLocaleTimeString()}</span>
+//           )}
+//         </p>
+//       );
+//     })}{" "}
+//   </div>
+
+//   <OptionsBar note={note} onEdit={onEdit} />
+// </div>;
+
