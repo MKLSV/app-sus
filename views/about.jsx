075d844e8ae95e8cba983532fbf693b9cd67dcaa
MKLSV <mkolosov96@gmail.com>
2022-12-31 20:27:02 +0300

    done--happy-new-yeear

--- views/about.jsx
+++ views/about.jsx
@@ -1,7 +1,8 @@
 
 export function About() {
     return <section className="about">
-        <h1 className="about-head">About Appsus app this app you can check your mails and make your own notes</h1>
+        <h1 className="about-head">About Appsus app</h1>
+        <h1 className="about-head-subject">This app you can check your mails and make your own notes</h1>
         <h2 className="about-text">Appsus is a free to use app that allows you to check your mails and make notes. You can also organize your notes by creating different folders. The app has a simple interface that makes it easy to use.</h2>
         <h2 className="about-text">It is available for android devices and is compatible with most versions of android. The app is also available for iOS devices but you need to have at least iOS 10 installed on your device in order for the app to work properly</h2>
     </section>
