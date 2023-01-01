e192ce1fb071c9e86ea0143a5fa113225d3beca8
alexokin <alex.okin@gmail.com>
2023-01-01 08:41:36 +0200

    fixed images

--- apps/mail/cmps/mail-header.jsx
+++ apps/mail/cmps/mail-header.jsx
@@ -5,7 +5,7 @@ import { MailFilter } from "./mail-filter.jsx";
 export function MailHeader({ onSetFilter }) {
 
     return <header className='mail-header'>
-        <a className='mail-logo'><img src="../../../assets/img/mail-logo.png"/></a>
+        <a className='mail-logo'><img src="./assets/img/mail-logo.png" alt="" srcset=""/></a>
         <MailFilter onSetFilter={onSetFilter} />
     </header>
 }
